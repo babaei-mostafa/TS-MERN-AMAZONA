43b4925684659e8d4fdb47cb55ae1a1d4ba2d688
babaei-mostafa <mostafa.bsh@gmail.com>
2023-06-11 23:08:02 +0330

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All rights are reserved</footer>
+    </div>
+  )
+}
+
+export default App
