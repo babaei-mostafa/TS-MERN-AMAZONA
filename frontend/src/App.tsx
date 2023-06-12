ad5037fa353ade28c9292272effc04d71fcc688a
babaei-mostafa <mostafa.bsh@gmail.com>
2023-06-12 12:21:28 +0330

    Linsting Sample Products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   return (
     <div>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All rights are reserved</footer>
     </div>
   )
